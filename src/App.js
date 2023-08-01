28f26d8e8748cb4dce09642f4fe7cc1888add9ad
Ritik kumar <hiritikjannat.love@gmail.com>
2023-08-01 22:21:58 +0530

    added the signup and login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import Navigation from "./components/Header.jsx";
-import Content from "./components/Content.jsx";
-import Footer from "./components/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Header.js";
+import Content from "./components/Content.js";
+import Footer from "./components/Footer.js";
+import { Signup } from "./components/Signup.js";
+import { Login } from "./components/Login.js";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Content />
-      <Footer />
-
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navigation />
+              <Content />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
